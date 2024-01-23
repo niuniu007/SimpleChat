6946642fee69d04b37ad22fe003f93f1b9f09300
自由的歌 <450940118@qq.com>
2024-01-23 11:05:10 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { NConfigProvider } from 'naive-ui'
+import { NaiveProvider } from '@/components/common'
+import { useTheme } from '@/hooks/useTheme'
+import { useLanguage } from '@/hooks/useLanguage'
+
+const { theme, themeOverrides } = useTheme()
+const { language } = useLanguage()
+
+
+</script>
+
+<template>
+  <div class="absolute  flex  h-full  w-full" style="background-color:#dbd8d8;">
+    <NConfigProvider class="h-full" :theme="theme" :theme-overrides="themeOverrides" :locale="language">
+      <NaiveProvider>
+        <RouterView />
+      </NaiveProvider>
+    </NConfigProvider>
+  </div>
+</template>
